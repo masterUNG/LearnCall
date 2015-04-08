6508f371a2139c977ac6c46da4281797b2013d67
masterUNG <phrombutr@gmail.com>
2015-04-08 10:04:18 +0700

    Splash

---
+++ app/src/main/java/appewtc/masterung/learncall/AboutMeActivity.java
@@ -0,0 +1,24 @@
+package appewtc.masterung.learncall;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+
+public class AboutMeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_me);
+    }   // onCreate
+
+    public void clickAndroid(View view) {
+
+    }
+
+    public void clickKbank(View view) {
+        
+    }
+
+}   // Main Class
--- app/src/main/java/appewtc/masterung/learncall/MainActivity.java
+++ app/src/main/java/appewtc/masterung/learncall/MainActivity.java
@@ -1,13 +1,24 @@
 package appewtc.masterung.learncall;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    //Explicit
+    private int intMyIcon[];
+    private String strPhoneNumber[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +32,65 @@ public class MainActivity extends ActionBarActivity {
 
     private void createListView() {
 
-        int intMyIcon[] = new int[]{R.drawable.call_01, R.drawable.call_02, R.drawable.call_03, R.drawable.call_04,
+        intMyIcon = new int[]{R.drawable.call_01, R.drawable.call_02, R.drawable.call_03, R.drawable.call_04,
                 R.drawable.call_05, R.drawable.call_06, R.drawable.call_07, R.drawable.call_08, R.drawable.call_09,
                 R.drawable.call_10, R.drawable.call_11, R.drawable.call_12, R.drawable.call_13, R.drawable.call_14,
                 R.drawable.call_15, R.drawable.call_16};
 
+        strPhoneNumber = new String[]{"0818595309", "1677", "1192", "191", "1669", "022821815", "199",
+                "1129", "1584", "1195", "1125", "1188", "1556", "1808", "1193", "1860"};
+
         //Create Adapter
+
         MyAdapter objMyAdapter = new MyAdapter(getApplicationContext(), intMyIcon);
 
         //Create ListView
         ListView objListView = (ListView) findViewById(R.id.listView);
         objListView.setAdapter(objMyAdapter);
 
+        //Active Click on ListView
+        objListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Log.d("phone", "i ==> " + Integer.toString(i));
+
+                showAlertDialog(i);
+
+            }
+        });
+
     }   // createListView
+
+    private void showAlertDialog(final int intI) {
+
+        AlertDialog.Builder objBuilder = new AlertDialog.Builder(this);
+        objBuilder.setIcon(intMyIcon[intI]);
+        objBuilder.setMessage("Are You Sure to Call ?");
+        objBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                //Call
+                Intent objIntent = new Intent(Intent.ACTION_DIAL);
+                objIntent.setData(Uri.parse("tel:" + strPhoneNumber[intI]));
+                startActivity(objIntent);
+
+                Log.d("phone", "intI receive ==> " + Integer.toString(intI));
+                Log.d("phone", "Call ==> " + strPhoneNumber[0]);
+
+                dialogInterface.dismiss();
+            }
+        });
+        objBuilder.setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        objBuilder.show();
+
+    }   // showAlertDialog
 
 
     @Override
